6dfeea6aa22074b7fbef565f8b6ca1b072721f03
Andrew Apperley <andrew.apperley@shopify.com>
2019-06-21 13:44:34 -0400

    Got it running with scenekit

--- evmu-watch-WatchKit-Extension/InterfaceController.swift
+++ evmu-watch-WatchKit-Extension/InterfaceController.swift
@@ -8,45 +8,35 @@
 
 import WatchKit
 import Foundation
+import SpriteKit
 import UIKit
 
 
 class InterfaceController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
+	@IBOutlet weak var screen: WKInterfaceSCNScene!
+	let device = gyVmuDeviceCreate()
+	
+	override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+		guard let path = Bundle.main.path(forResource: "minigame", ofType: "dci") else {return}
+		var status: VMU_LOAD_IMAGE_STATUS! = VMU_LOAD_IMAGE_STATUS(rawValue: 0)
+		gyVmuFlashLoadImage(device, path, &status)
+		
+		let scene = SCNScene()
+		let dummyAction = SCNAction.scale(by: 1.0, duration: 1.0)
+		let repeatAction = SCNAction.repeatForever(dummyAction)
+		scene.rootNode.runAction(repeatAction)
+		screen.scene = scene
+		screen.delegate = self
+		
+		
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-//		let device = gyVmuDeviceCreate()
-		testAssetLoading()
-		guard let vmsBundleURL = Bundle.main.url(forResource: "minigame", withExtension: "VMS") else {return}
-//		guard let vmiBundleURL = Bundle.main.url(forResource: "minigame", withExtension: "VMI") else {return}
-		
-//		let vmsExists = FileManager.default.fileExists(atPath: vmsBundleURL.absoluteString)
-//		let vmiExists = FileManager.default.fileExists(atPath: vmiBundleURL.absoluteString)
-//		var fileSize: Int = 0
-//		var fileSizePointer = UnsafeMutablePointer<Int>?(&fileSize)
-//		var pathPointer = UnsafePointer<Int8>(vmsBundleURL.absoluteString)
-//		var status: VMU_LOAD_IMAGE_STATUS! = VMU_LOAD_IMAGE_STATUS(rawValue: 0)
-//		let dir = gyVmuFlashLoadVMS(pathPointer, fileSizePointer)
-//
-//		while(true) {
-//			gyVmuDeviceUpdate(device, 1)
-//			for (i, h) in (0..<VMU_DISP_PIXEL_HEIGHT).enumerated() {
-//				for (ii, w) in (0..<VMU_DISP_PIXEL_WIDTH).enumerated() {
-//					if(gyVmuDisplayPixelGet(device, w, h) > 0) {
-//						print("black pixel")
-//					} else {
-//						print("white pixel")
-//					}
-//				}
-//			}
-//		}
 //		gyVmuDeviceDestroy(device)
     }
     
@@ -55,21 +45,25 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
-	func testAssetLoading() {
-		guard let vmsBundleURL = Bundle.main.url(forResource: "minigame", withExtension: "VMS") else {return}
-	}
-	
 }
-//
-//UIGraphicsBeginImageContextWithOptions(CGSize(width: WIDTH, height: HEIGHT), true, 1.5)
-//guard let context = UIGraphicsGetCurrentContext() else {return}
-//for (i, data) in vram.enumerated() {
-//	let colour: UIColor = data <= 0 ? .black : .white
-//	let x = i % Int(WIDTH)
-//	let y = (i - x) / Int(WIDTH)
-//	let rect = CGRect(x: x, y: y , width: 1, height: 1)
-//	context.setFillColor(colour.cgColor)
-//	context.fill(rect)
-//}
-//(self.screen.subviews[0] as! UIImageView).image = UIGraphicsGetImageFromCurrentImageContext()
-//UIGraphicsEndImageContext()
+
+extension InterfaceController: SCNSceneRendererDelegate {
+	func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+		guard let scene = self.screen.scene else {
+			return
+		}
+		gyVmuDeviceUpdate(device, 1)
+		UIGraphicsBeginImageContextWithOptions(CGSize(width: CGFloat(VMU_DISP_PIXEL_WIDTH), height: CGFloat(VMU_DISP_PIXEL_HEIGHT)), true, 1)
+		guard let context = UIGraphicsGetCurrentContext() else {return}
+		for y in (0..<VMU_DISP_PIXEL_HEIGHT) {
+			for x in (0..<VMU_DISP_PIXEL_WIDTH) {
+				let rect = CGRect(x: Int(x), y: Int(y), width: 1, height: 1)
+				context.setFillColor(gyVmuDisplayPixelGet(device, x, y) > 0 ? UIColor.black.cgColor : UIColor.white.cgColor)
+				context.fill(rect)
+			}
+		}
+		
+		scene.background.contents = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+	}
+}
